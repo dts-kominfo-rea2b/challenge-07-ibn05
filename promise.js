78d6580cd6dde38d8f868f658a2a0fd3fa9ab81e
ibn05 <arikibnur@gmail.com>
2022-06-12 17:47:03 +0700

    Completed the challenge

--- promise.js
+++ promise.js
@@ -1,8 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+    try {
+        // Get data from all Cinemas API
+        const [dataIXX, dataVGC] = await Promise.all([
+            promiseTheaterIXX(),
+            promiseTheaterVGC(),
+        ]);
+
+        //Count the type of emotion based on the parameter
+        return [...dataIXX, ...dataVGC]?.filter(
+            (item) => item.hasil === emotion
+        ).length;
+    } catch (e) {
+        console.log("Oops, there was an error: " + e);
+    }
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
